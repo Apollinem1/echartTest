7589037a5e9d34b0f98c5d006ff2cd5d554e59cb
Apollinem <rossamahinnikita@gmail.com>
2025-02-21 19:24:59 +0700

    added flag for chaging from real api to mocked data

--- src/services/ChartDataService.ts
+++ src/services/ChartDataService.ts
@@ -1,19 +1,35 @@
 import axios from "axios";
+import { generateNormalDistributionData } from "../utils/dataGenerators";
+const useMockedData = false;
 
 export class ChartDataService {
   async getMilkStats(): Promise<number[]> {
-    const res = await axios.get("/api/milk-stats");
-    return res.data.data;
+    if (useMockedData) {
+      // Блок генерации
+      return generateNormalDistributionData(12, 200, 30);
+    } else {
+      // Вызов реального API
+      const res = await axios.get("/api/milk-stats"); //опять же в дев режиме подключение к моковому апи
+      return res.data.data;
+    }
   }
 
   async getCheeseStats(): Promise<number[]> {
-    const res = await axios.get("/api/cheese-stats");
-    return res.data.data;
+    if (useMockedData) {
+      return generateNormalDistributionData(12, 80, 15);
+    } else {
+      const res = await axios.get("/api/cheese-stats");
+      return res.data.data;
+    }
   }
 
   async getYogurtStats(): Promise<number[]> {
-    const res = await axios.get("/api/yogurt-stats");
-    return res.data.data;
+    if (useMockedData) {
+      return generateNormalDistributionData(12, 50, 10);
+    } else {
+      const res = await axios.get("/api/yogurt-stats");
+      return res.data.data;
+    }
   }
 }
 
